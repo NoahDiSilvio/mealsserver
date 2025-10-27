c4f346b5a4c91f1445624074a37bd1fc08b88d9d
NoahDiSilvio <129743471+NoahDiSilvio@users.noreply.github.com>
2025-10-27 22:16:38 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,7 @@
-import pg from 'pg';
-const { Pool } = pg;
-
-const pool = new Pool({
-    connectionString: process.env.DB_URL,
-    ssl:{
-        rejectUnauthorized: false
-    }
+import * as mysql from "mysql2/promise";
+export const pool = mysql.createPool({
+    host:'sql7.freesqldatabase.com',
+    user:'sql7804775',
+    password:'ubnQxYw8mA',
+    database: 'sql7804775'
 });
-export default pool;
